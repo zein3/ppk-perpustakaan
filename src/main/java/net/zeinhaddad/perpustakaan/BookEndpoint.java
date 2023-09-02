60428110b4bcb91cb84d0116402f3416e591d7af
zein3 <zeinhaddad02@gmail.com>
2023-09-02 11:21:13 +0700

    finished service getBooks

--- src/main/java/net/zeinhaddad/perpustakaan/BookEndpoint.java
+++ src/main/java/net/zeinhaddad/perpustakaan/BookEndpoint.java
@@ -1,5 +1,7 @@
 package net.zeinhaddad.perpustakaan;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -8,8 +10,10 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import net.zeinhaddad.perpustakaan.dto.BookDto;
 import net.zeinhaddad.perpustakaan.service.BookService;
+import net.zeinhaddad.perpustakaan.soap.Book;
 import net.zeinhaddad.perpustakaan.soap.CreateBookRequest;
 import net.zeinhaddad.perpustakaan.soap.CreateBookResponse;
+import net.zeinhaddad.perpustakaan.soap.GetBooksResponse;
 
 @Endpoint
 public class BookEndpoint {
@@ -30,6 +34,24 @@ public class BookEndpoint {
 
         CreateBookResponse response = new CreateBookResponse();
         response.setStatusCode(201);
+        return response;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getBooksRequest")
+    @ResponsePayload
+    public GetBooksResponse getBooks() {
+        GetBooksResponse response = new GetBooksResponse();
+
+        List<BookDto> books = bookService.getBooks();
+        for (BookDto book : books) {
+            Book it = new Book();
+            it.setTitle(book.getTitle());
+            it.setAuthor(book.getAuthor());
+            it.setDescription(book.getDescription());
+            
+            response.getBooks().add(it);
+        }
+
         return response;
     }
 }
